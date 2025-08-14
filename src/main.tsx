70f18a9edd036d5c630f479e60d490e29f5570ef
Iliias Bayishev <168758422+Iliiasik@users.noreply.github.com>
2025-08-14 17:42:21 +0600

    Бесшовный рефреш, теперь все операции с токенами работают четко

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App.tsx';
 import { UserProvider } from '@shared/context/UserContext.tsx';
 import { ToastProvider } from '@shared/context/ToastContext';
+import { initTokenRefresh } from '@shared/auth/tokenRefresher';
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <UserProvider>
-            <ToastProvider>
-                <App />
-            </ToastProvider>
-        </UserProvider>
-    </StrictMode>
-);
+async function bootstrap() {
+    await initTokenRefresh();
+
+    createRoot(document.getElementById('root')!).render(
+        <StrictMode>
+            <UserProvider>
+                <ToastProvider>
+                    <App />
+                </ToastProvider>
+            </UserProvider>
+        </StrictMode>
+    );
+}
+
+bootstrap();
--- src/shared/axios/axios.ts
+++ src/shared/axios/axios.ts
@@ -5,29 +5,4 @@ const api = axios.create({
     withCredentials: true,
 });
 
-api.interceptors.response.use(
-    response => response,
-    async error => {
-        const originalRequest = error.config;
-
-        if (error.response.status === 401 && !originalRequest._retry) {
-            originalRequest._retry = true;
-            console.log('Attempting token refresh...');
-
-            try {
-                const response = await axios.post('/api/auth/refresh', null, {
-                    withCredentials: true
-                });
-                console.log('Refresh successful', response.data);
-                return api(originalRequest);
-            } catch (refreshError) {
-                console.error('Refresh failed', refreshError);
-                return Promise.reject(error);
-            }
-        }
-
-        return Promise.reject(error);
-    }
-);
-
 export default api;
